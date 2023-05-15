3e63e0db5698e63e1f96d92a02e632e7c044ddbe
Sahil Dussa <sahildussa457@gmail.com>
2023-05-16 04:40:11 +0530

    fix loading component

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,9 +1,5 @@
-import React, { useEffect } from "react";
 import loading from "../assets/Loading.gif";
 const Loading = () => {
-  useEffect(() => {
-    console.log("Component rendered");
-  }, []);
   return (
     <div>
       <img src={loading} alt="Loading Animation" />
